4fcd7a35d0fad2037af4956e47bc7134784c275f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-14 18:02:08 -0500

    Checking in progress, but unable to see sets at this time

--- db/migrate/20210113234642_create_sets.rb
+++ db/migrate/20210113234642_create_sets.rb
@@ -1,7 +1,8 @@
 class CreateSets < ActiveRecord::Migration[6.0]
   def change
     create_table :sets do |t|
-
+      t.string :exercise_type
+      t.decimal :rep_count
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_233456) do
+ActiveRecord::Schema.define(version: 2021_01_13_234642) do
+
+  create_table "sets", force: :cascade do |t|
+    t.string "exercise_type"
+    t.decimal "rep_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "workouts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
