992c73fbc1b100453d59288c86b103d97eb460d7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-14 18:52:34 -0500

    Proved save

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -1,16 +1,47 @@
 class ExerciseSetsController < ApplicationController
+  before_action :get_exercise_set, only: %i[show edit delete update]
+  
   def new
+    @exercise_set = ExerciseSet.new
   end
 
   def create
+    @exercise_set = ExerciseSet.new(exercise_set_params)
+    if @exercise_set.save
+      redirect_to @exercise_set
+    else
+      flash[:error] = "Didn't save"
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def destroy
+    ExerciseSet.find_by(params)
+
+    ExerciseSet.destroy
   end
 
   def update
+   
+  end
+
+  def show
+    
+  end
+
+  def exercise_set_params
+    params.require(:exercise_set).permit(
+      :exercise_type,
+      :rep_count
+    )
+  end
+
+  private
+  def get_exercise_set
+    @exercise_set = ExerciseSet.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'exercise_sets/edit'
   get 'exercise_sets/destroy'
   get 'exercise_sets/update'
+  post 'exercise_sets/create', to: 'exercise_sets#create'
+  resources :exercise_sets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
