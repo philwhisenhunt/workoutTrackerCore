337caba55ffb18eadeffc60dbde6fa3eadaa0084
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-19 18:49:10 -0500

    Make the home page show up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'exercise_sets/edit'
   get 'exercise_sets/destroy'
   get 'exercise_sets/update'
+  root to: 'pages#main'
   post 'exercise_sets/create', to: 'exercise_sets#create'
   resources :exercise_sets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
