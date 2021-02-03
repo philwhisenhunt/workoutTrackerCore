59362d256e6746b406fbb6c797707eae5d4d084c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-02 22:48:53 -0500

    Resolve changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
 # before_action :set_cors
 
-    def set_cors
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
+skip_before_action :verify_authenticity_token
+
 end
