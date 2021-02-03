ead464680f10631aa7bbeadd99c92e8005d35797
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-02 22:48:26 -0500

    Add root, remove cruft

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'exercise_sets/new'
-  get 'exercise_sets/create'
-  get 'exercise_sets/edit'
-  get 'exercise_sets/destroy'
-  get 'exercise_sets/update'
-  root to: 'pages#main'
-  post 'exercise_sets/create', to: 'exercise_sets#create'
+
   resources :exercise_sets
+  root to: 'pages#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
