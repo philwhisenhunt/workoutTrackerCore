3114bf2adf8ac044a4be4086cbd4758c5fb6f4ce
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-09 21:01:50 -0500

    Fix table names spelling, migrations removed errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_205539) do
+ActiveRecord::Schema.define(version: 2021_03_10_020030) do
 
   create_table "exercise_sets", force: :cascade do |t|
     t.string "exercise_type"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_205539) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "weight"
-    t.datetime "occured"
+    t.datetime "occurred"
   end
 
   create_table "users", force: :cascade do |t|
